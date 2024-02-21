64f45759815e87ec28732493cec7c41ff0e6a9f4
Stephen Huan <stephen03.huan@gmail.com>
2024-02-20 21:32:00 -0500

    style(python/main.py): black + isort

--- python/main.py
+++ python/main.py
@@ -1,10 +1,13 @@
 import time
+
+import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.gaussian_process.kernels as kernels
-import matplotlib.pyplot as plt
-import pysensor as sensor
-import cysensor
 
+import cysensor
+import pysensor as sensor
+
+# fmt: off
 lightblue     = "#a1b4c7"
 orange        = "#ea8810"
 silver        = "#b0aba8"
@@ -17,32 +20,42 @@ darksilver    = "#96918f"
 darklightblue = "#8999a9"
 darkrust      = "#9c380d"
 darkseagreen  = "#1e4833"
+# fmt: on
 
-POINT_SIZE = 20 # point sizes
-BIG_POINT  = 40 # large point
+POINT_SIZE = 20  # point sizes
+BIG_POINT = 40  # large point
 
-def grid(n: int, a: float=0, b: float=1, d: int=2) -> np.ndarray:
-    """ Generate n points evenly spaced in a [a, b]^d hypercube. """
-    spaced = np.linspace(a, b, round(n**(1/d)))
-    cube = (spaced,)*d
+
+def grid(n: int, a: float = 0, b: float = 1, d: int = 2) -> np.ndarray:
+    """Generate n points evenly spaced in a [a, b]^d hypercube."""
+    spaced = np.linspace(a, b, round(n ** (1 / d)))
+    cube = (spaced,) * d
     return np.stack(np.meshgrid(*cube), axis=-1).reshape(-1, d)
 
-def perturbed_grid(rng: np.random.Generator, n: int, a: float=0, b: float=1,
-                   d: int=2, delta: float=None) -> np.ndarray:
-    """ Generate n points roughly evenly spaced in a [a, b]^d hypercube. """
+
+def perturbed_grid(
+    rng: np.random.Generator,
+    n: int,
+    a: float = 0,
+    b: float = 1,
+    d: int = 2,
+    delta: float | None = None,
+) -> np.ndarray:
+    """Generate n points roughly evenly spaced in a [a, b]^d hypercube."""
     points = grid(n, a, b, d)
     # compute level of perturbation as half width
     if delta is None:
         # one point, width ill-defined
-        width = (b - a)/(n**(1/d) - 1) if n**(1/d) > 1 else 0
-        delta = 1/2*width
-    return points + rng.uniform(-delta, delta, points.shape)
+        width = (b - a) / (n ** (1 / d) - 1) if n ** (1 / d) > 1 else 0
+        delta = 1 / 2 * width
+    return points + rng.uniform(-delta, delta, points.shape)  # type: ignore
+
 
 if __name__ == "__main__":
     np.set_printoptions(precision=3, suppress=True)
     rng = np.random.default_rng(1)
 
-    kernel = kernels.Matern(length_scale=1, nu=5/2)
+    kernel = kernels.Matern(length_scale=1, nu=5 / 2)
     X = rng.random((100, 2))
     s = 75
 
@@ -72,23 +85,43 @@ if __name__ == "__main__":
 
     # graphing
 
-    kernel = kernels.Matern(length_scale=1, nu=5/2)
+    kernel = kernels.Matern(length_scale=1, nu=5 / 2)
 
     n = 10
     s = 10
-    X = grid(n*n, 0, 1)
+    X = grid(n * n, 0, 1)
     entropy = sensor.entropy_naive(X, kernel, s)
     mi = sensor.mi_naive(X, kernel, s)
 
     # plt.style.available for available themes
     plt.style.use("seaborn-v0_8-paper")
 
-    plt.scatter(X[:, 0], X[:, 1], label="candidates",
-                zorder=1, s=POINT_SIZE, color=silver)
-    plt.scatter(X[entropy, 0], X[entropy, 1], label="entropy",
-                zorder=2, s=BIG_POINT, marker="D", color=lightblue)
-    plt.scatter(X[mi, 0], X[mi, 1], label="mutual info",
-                zorder=3, s=BIG_POINT, marker="s", color=rust)
+    plt.scatter(
+        X[:, 0],
+        X[:, 1],
+        label="candidates",
+        zorder=1,
+        s=POINT_SIZE,
+        color=silver,
+    )
+    plt.scatter(
+        X[entropy, 0],
+        X[entropy, 1],
+        label="entropy",
+        zorder=2,
+        s=BIG_POINT,
+        marker="D",
+        color=lightblue,
+    )
+    plt.scatter(
+        X[mi, 0],
+        X[mi, 1],
+        label="mutual info",
+        zorder=3,
+        s=BIG_POINT,
+        marker="s",
+        color=rust,
+    )
     plt.gca().set_aspect("equal")
     plt.legend()
     plt.savefig("data/sensor.png", bbox_inches="tight")
@@ -102,26 +135,54 @@ if __name__ == "__main__":
     # n = 10
     # s = 10
     # X = grid(n*n, 0, 1)
-    mu = np.zeros(n*n)
+    mu = np.zeros(n * n)
     # rng = np.random.default_rng(1)
     y = rng.multivariate_normal(mu, kernel(X))
 
     entropy = sensor.entropy_naive(X, kernel, s)
     mi = sensor.mi_naive(X, kernel, s)
 
-    surf = ax.plot_surface(
-        X[:, 0].reshape(n, n), X[:, 1].reshape(n, n), y.reshape(n, n),
-        label="measurements", zorder=0, color=silver, alpha=0.5
+    surf = ax.plot_surface(  # type: ignore
+        X[:, 0].reshape(n, n),
+        X[:, 1].reshape(n, n),
+        y.reshape(n, n),
+        label="measurements",
+        zorder=0,
+        color=silver,
+        alpha=0.5,
     )
     # https://stackoverflow.com/questions/54994600/65554278#65554278
     surf._facecolors2d = surf._facecolor3d
     surf._edgecolors2d = surf._edgecolor3d
-    ax.scatter(X[:, 0], X[:, 1], y, label="candidates",
-               zorder=1, s=POINT_SIZE, color=silver)
-    ax.scatter(X[entropy, 0], X[entropy, 1], y[entropy], label="entropy",
-               zorder=2, s=BIG_POINT, marker="D", color=lightblue)
-    ax.scatter(X[mi, 0], X[mi, 1], y[mi], label="mutual info",
-               zorder=3, s=BIG_POINT, marker="s", color=rust)
+    ax.scatter(
+        X[:, 0],
+        X[:, 1],
+        y,
+        label="candidates",
+        zorder=1,
+        s=POINT_SIZE,  # pyright: ignore
+        color=silver,
+    )
+    ax.scatter(
+        X[entropy, 0],
+        X[entropy, 1],
+        y[entropy],
+        label="entropy",
+        zorder=2,
+        s=BIG_POINT,  # pyright: ignore
+        marker="D",
+        color=lightblue,
+    )
+    ax.scatter(
+        X[mi, 0],
+        X[mi, 1],
+        y[mi],
+        label="mutual info",
+        zorder=3,
+        s=BIG_POINT,  # pyright: ignore
+        marker="s",
+        color=rust,
+    )
     plt.legend()
     plt.savefig("data/gp.png", bbox_inches="tight")
     ax.clear()
@@ -180,4 +241,3 @@ if __name__ == "__main__":
     indexes = sensor.mi_chol(X, kernel, s)
     t2 = time.time() - start
     print(f"python     chol: {t2:6.3f} ({t1/t2:7.3f})")
-
